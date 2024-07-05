67f16bc1db6ccfee1abf54022b7bcf5d3242e179
Tim L <timl@Zipcoders-MacBook-Pro.local>
2024-07-05 15:49:46 -0400

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,39 @@ public class Person {
     private String name;
     private int age;
 
+
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
+
     }
 
     public Person(int age) {
+        this.age = 5;
     }
 
     public Person(String name) {
+        this.name = "Leon";
     }
 
     public Person(String name, int age) {
+        this.name = "Leon";
+        this.age = 5;
     }
 
     public void setName(String name) {
+        this.name = "Leon";
     }
 
     public void setAge(int age) {
+        this.age = 5;
     }
 
     public String getName() {
-        return null;
+        return name;
+    }
+    public Integer getAge(){
+        return age;
     }
 
-    public Integer getAge() {
-        return null;
-    }
 }
